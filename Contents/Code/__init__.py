e9ddcaa9c62c8abef435dbe08187745919d1d4fa
Furs <force@power-net.org>
2013-07-08 21:24:19 +0200

    seems i forgot to commit this one :x

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -19,9 +19,6 @@ structure = 'stream/channelid'
 transcode = '?mux=matroska&acodec=vorbis&vcodec=H264&scodec=NONE&transcode=1&resolution=384' ## Proof Of Concept
 htsurl = 'http://%s:%s@%s:%s/%s/' % (username, password, hostname, web_port, structure)
 
-#Resource Files
-xml_file = Resource.Load('channels.xml')
-
 #Texts
 TEXT_TITLE = u'HTS-TVheadend'
 TEXT_CHANNELS = u'Channels'
@@ -41,54 +38,59 @@ def MainMenu():
 
 	menu = ObjectContainer(title1=TEXT_TITLE)
 	menu.add(DirectoryObject(key=Callback(GetChannels, prevTitle=TEXT_TITLE), title=TEXT_CHANNELS,))
+	menu.add(DirectoryObject(key=Callback(GetbyTags, prevTitle=TEXT_TITLE), title='tags',))
 	menu.add(PrefsObject(title='Preferences'))
 
 	return menu
 
-def getChannelsjson(username,password,host,port):
+def getTVHeadendJson(what):
+	tvh_url = dict( channels='op=list', channeltags='op=listTags')
         base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
-        request = urllib2.Request("http://%s:%s/channels" % (host,port),"op=list" )
+        request = urllib2.Request("http://%s:%s/%s" % (hostname,web_port,what),tvh_url[what])
         request.add_header("Authorization", "Basic %s" % base64string)
         response = urllib2.urlopen(request)
         json_tmp = response.read()
         json_data = json.loads(json_tmp)
         return json_data
 
-if "on" in options_transcode:
-	def GetChannels(prevTitle):
- 		json_data = getChannelsjson(username,password,hostname,web_port)
-		channelList = ObjectContainer(title1=prevTitle, title2=TEXT_CHANNELS)
-		
-		for channel in json_data['entries']:
-			name = channel['name']
-			id = channel['chid']
-			if 'ch_icon' in channel:
-				icons = channel['ch_icon']
-			else:
-				icons = ''
-                	mo = MediaObject(parts=[PartObject(key=HTTPLiveStreamURL("%s%s%s" % (htsurl, id, transcode)))])
-                	vco = VideoClipObject(title=name, thumb=icons, url='%s%s%s' % (htsurl, id, transcode))
-                	vco.add(mo)
-                	channelList.add(vco)
-        	return channelList
+def GetbyTags(prevTitle):
+	json_data = getTVHeadendJson('channeltags')
+	tagList = ObjectContainer(title1=prevTitle, title2='tags')
 
-else:
-	def GetChannels(prevTitle):
-		json_data = getChannelsjson(username,password,hostname,web_port)
-		channelList = ObjectContainer(title1=prevTitle, title2=TEXT_CHANNELS)
-		
-		for channel in json_data['entries']:
+	for tag in json_data['entries']:
+		tagList.add(DirectoryObject(key=Callback(GetChannels, prevTitle=tag['name'], tag=int(tag['identifier'])), title=tag['name'],))
+	return tagList
+
+def GetChannels(prevTitle, tag=int(0)):
+	json_data = getTVHeadendJson('channels')
+	channelList = ObjectContainer(title1=prevTitle, title2=TEXT_CHANNELS)
+
+	for channel in json_data['entries']:
+		name = ''
+		if tag > 0:
+			tags = channel['tags'].split(',')
+			for tids in tags:
+				if (tag == int(tids)):
+					name = channel['name']
+					id = channel['chid']
+		else:
 			name = channel['name']
 			id = channel['chid']
+
+		if name != '':
 			if 'ch_icon' in channel:
 				icons = channel['ch_icon']
 			else:
 				icons = ''
-			mo = MediaObject(parts=[PartObject(key=HTTPLiveStreamURL("%s%s" % (htsurl, id)))])
-			vco = VideoClipObject(title=name, thumb=icons, url='%s%s' % (htsurl, id))
+			if "on" in options_transcode:
+				mo = MediaObject(parts=[PartObject(key=HTTPLiveStreamURL("%s%s%s" % (htsurl, id, transcode)))])
+				vco = VideoClipObject(title=name, thumb=icons, url='%s%s%s' % (htsurl, id, transcode))
+			else:
+				mo = MediaObject(parts=[PartObject(key=HTTPLiveStreamURL("%s%s" % (htsurl, id)))])
+				vco = VideoClipObject(title=name, thumb=icons, url='%s%s' % (htsurl, id))
 			vco.add(mo)
 			channelList.add(vco)
-        	return channelList
+       	return channelList
 
 #################################################
 #Examples
